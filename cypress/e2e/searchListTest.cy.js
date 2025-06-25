52b00e2fd9510e8a84e796e132775c3dceddb501
Palomadev <paloma166.pah@gmail.com>
2025-06-25 12:29:09 -0300

    att arquivos

---
+++ cypress/e2e/searchListTest.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+const { categories } = require("../fixtures/categories.json");
+const { email, senha } = require("../fixtures/data.json");
+const { homePage } = require("../support/pages/home.page");
+
+describe('List Products', () => {
+
+    beforeEach(() => {
+        cy.login(email, senha)
+    })
+
+    it(`should search product`, () => {
+        homePage.openSearchProduct()
+        homePage.searchProduct('in')
+        homePage.products().should('have.length.greaterThan', 0)
+
+        homePage.products().each(product=>{
+            let price = product.find('[data-testid="price"]').text()
+            expect(price).to.contain('R$')
+        })
+
+        cy.compareSnapshot(Cypress.currentTest.title, 0)
+    });
+
+})
